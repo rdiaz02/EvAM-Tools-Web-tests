e2654ea5c610a65fa8bcb53d7b54664c35a20984
LorenAdet <loren_AD@outlook.com>
2023-03-30 21:41:32 +0200

    new test functions

--- src/selenium_web_driver.py
+++ src/selenium_web_driver.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 import os
+import time
 
 # Geckodriver moved to /usr/bin/
 dotenv_path = Path('path/to/.env')
@@ -14,6 +15,8 @@ load_dotenv(dotenv_path=dotenv_path)
 
 class selenium_driver():
     driver = None
+    actual_tab = None
+    url = os.getenv('EVAM_URL')
 
     def connect(self) -> None:
 
@@ -27,14 +30,24 @@ class selenium_driver():
         firefox_binary.add_command_line_options("-private")
 
         self.driver = webdriver.Firefox(firefox_profile=profile, options=options, firefox_binary=firefox_binary)
-        self.driver.get('https://www.iib.uam.es/evamtools/')
-        self.driver.maximize_window()
+        self.driver.get(self.url)
+
+    # Miscelaneous functions
 
     def close(self) -> None:
         if self.driver != None:
             self.driver.close()
 
-    def wait_visibility_by_xpath(self, xpath, timeout=10) -> bool:
+    def wait_invisibility_by_xpath(self, xpath, timeout) -> bool:
+        try:
+            WebDriverWait(self.driver, timeout).until(
+                ec.invisibility_of_element_located((By.XPATH, xpath))
+            )
+            return True
+        except Exception:
+            return False
+
+    def wait_visibility_by_xpath(self, xpath, timeout) -> bool:
         try:
             WebDriverWait(self.driver, timeout).until(
                 ec.visibility_of_element_located((By.XPATH, xpath))
@@ -43,6 +56,43 @@ class selenium_driver():
         except Exception:
             return False
 
-    def find_and_click(self, xpath) -> None:
-        elem = self.driver.find_element_by_xpath(xpath)
-        elem.click()
+    def find_and_click(self, xpath, timeout=10) -> None:
+        if self.wait_visibility_by_xpath(xpath, timeout):
+            elem = self.driver.find_element_by_xpath(xpath)
+            elem.click()
+        else:
+            print('Element not found')
+
+    # test oriented functions
+
+    def test_navbar_load_page(self) -> None:
+        self.navbar = self.driver.find_element_by_id('navbar')
+        self.session_id = self.navbar.get_attribute('data-tabsetid')
+
+        for idx, tab in enumerate(self.navbar.find_elements_by_tag_name('li')):
+            nav_bar_button = tab.find_element_by_tag_name('a')
+            if nav_bar_button.get_attribute('href') == f"{self.url}#tab-{self.session_id}-{str(idx+1)}":
+                nav_bar_button.click()
+
+    def navbar_to(self, tab_name, timeout) -> None:
+        start = time.time()
+        for tab in self.navbar.find_elements_by_tag_name('li'):
+            if tab.find_element_by_tag_name('a').text == tab_name:
+                while tab.get_attribute('class') != 'active':
+                    tab.click()
+                    end = time.time()
+                    if end - start > timeout:
+                        return 'Failed'
+                break
+        self.actual_tab = tab_name
+        return 'Success'
+
+    def loading_content(self) -> None:
+        page_content = self.driver.find_element_by_class_name('container-fluid')
+        for elem in page_content:
+            try:
+                content = elem.find_element_by_class_name('tab-content')
+            except Exception as e:
+                print(e)
+
+        print('Loading finished')
