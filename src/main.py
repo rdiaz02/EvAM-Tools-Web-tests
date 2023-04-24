a2d73b5bb6587fcf8cca2490bfd463c1924c7067
LorenAdet <loren_AD@outlook.com>
2023-04-24 21:55:19 +0200

    feat: example main

--- src/main.py
+++ src/main.py
@@ -1,19 +1,9 @@
-from selenium_web_driver import selenium_driver
-from tests import tester
+from selenium_tests import *
+
+
+def test_evam_tools_page():
+    test_basic = TestEvamToolsWebPage()
+    test_basic.run()
 
 if __name__ == '__main__':
-    web_driver = selenium_driver()
-    web_driver.connect()
-    web_driver.test_navbar_load_page()
-    print(web_driver.navbar_to('User input', 10))
-    print(web_driver.navbar_to('Results', 10))
-    print(web_driver.navbar_to('User input', 10))
-    print(web_driver.navbar_to('About EvAM-Tools', 10))
-    # ret = web_driver.move_to_user_input(10)
-    # web_driver.loading_content()
-
-    #test_runner = tester(web_driver)
-    #test_runner.analyzing_BRCA_data_set()
-    #test_runner.analyzing_manually_constructed_synthetic_data()
-    #test_runner.small_computational_experiments()
-    #test_runner.analyzing_manually_constructed_data()
+    test_evam_tools_page()
