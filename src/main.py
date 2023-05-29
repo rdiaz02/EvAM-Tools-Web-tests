73d543db7e8d3f58749cf44b1f12eff4febcd3fc
LorenAdet <loren_AD@outlook.com>
2023-05-30 00:22:30 +0200

    refactor: main files

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from selenium_tests import *
-
-
-def test_evam_tools_page():
-    test_basic = TestEvamToolsWebPage()
-    test_basic.run()
-
-if __name__ == '__main__':
-    test_evam_tools_page()
