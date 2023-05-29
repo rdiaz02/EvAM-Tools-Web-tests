5e11b30345220d2c25c9f0b1b44f1a4455f8ae41
LorenAdet <loren_AD@outlook.com>
2023-05-30 00:23:04 +0200

    feat: arguments from config file implemented

--- src/test_case.py
+++ src/test_case.py
@@ -1,32 +1,47 @@
-import unittest
-from evam_tools_driver import EvAMToolsDriver
-from evam_tools_page_tests import EvAMToolsPage
+import traceback
 
-class TestCase(unittest.TestCase):
-    
-    def __init__(self, tests_list):
-        self.tests_list = tests_list
-        self.drive = None
+from selenium_core.selenium_wrapper import WrappedDriver
+from selenium_core.evam_tools_page_controller import EvAMToolsPage
+
+
+class TestCase:
+    def __init__(self, name, headless, maximize, large_log, screenshots):
+        self.name = name
+        self.headless = headless
+        self.maximize = maximize
+        self.large_log = large_log
+        self.screenshots = screenshots
+        self.wrapped_driver = None
         self.page_controller = None
 
+    def test_body(self):
+        # Override this method in the test case
+        print("Default test body: Override this method in the test case")
+        pass
+
     def run(self):
-        print('Running tests: ' + str(self.tests_list))
-        for test in self.tests_list:
-            self.setUp()
-            try:
-                method = getattr(self, test)
-                method()
-            except Exception as e:
-                print('Error running test ' + test + ': ' + str(e))
-            self.tearDown()
+        self.set_up()
 
-    def setUp(self):
-        self.driver = EvAMToolsDriver()
-        self.driver.connect(headless=False, maximize=True)
-        self.page_controller = EvAMToolsPage(self.driver.get_driver())
+        try:
+            method = getattr(self, "test_body")
+            method()
+        except Exception as e:
+            print(f"Error while running test: {e}\n")
 
-    def tearDown(self):
+            if self.large_log:
+                print(traceback.format_exc())
+
+            if self.screenshots:
+                self.wrapped_driver.save_screenshot(str(self.name) + "_error.png")
+
+        self.tear_down()
+
+    def set_up(self):
+        self.wrapped_driver = WrappedDriver(self.headless, self.maximize)
+        self.wrapped_driver.connect()
+        self.page_controller = EvAMToolsPage(
+            self.wrapped_driver.get_driver(), self.large_log, self.screenshots
+        )
+
+    def tear_down(self):
         self.driver.close()
-
-    def test_default(self):
-        print('test_default')
