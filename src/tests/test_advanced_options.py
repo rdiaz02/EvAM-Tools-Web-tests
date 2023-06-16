27f8deb732eebf0319bfbfdec3f2e44b3b7239bf
LorenAdet <loren_AD@outlook.com>
2023-06-16 18:45:33 +0200

    feat: new tests implemented

--- src/tests/test_advanced_options.py
+++ src/tests/test_advanced_options.py
@@ -8,6 +8,7 @@ class TestMethod(TestCase):
     def test_body(self):
         self.evam_driver.navbar_to("User input", 10)
         self.evam_driver.toogle_advanced_options_on()
-        self.evam_driver.set_advanced_options("Return paths to maximum(a)", "TRUE")
-        self.evam_driver.set_advanced_options("Lambda:", "9")
+        self.evam_driver.set_advanced_options("OncoBN_model-label", "Conjunctive (CBN)")
+        self.evam_driver.set_advanced_options("return_paths_max-label", "TRUE")
+        self.evam_driver.set_advanced_options("MHN_lambda-label", "9")
         self.evam_driver.toogle_advanced_options_off()
